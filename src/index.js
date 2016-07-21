142cf2061b8be17f41a2633033b02506d36043d1
Marc Riera <mrc2407@gmail.com>
2016-07-21 14:40:10 +0200

    Control loop with a click

--- src/index.js
+++ src/index.js
@@ -6,11 +6,33 @@ import { turn } from './events/turn'
 
 document.getElementById('log_details').innerHTML = '';
 document.getElementById('settlers_details').innerHTML = '';
+let delay = 500;
+let timeoutId;
+let loopOn = true;
 
 let settlement = makeSettlement({food: 500000, people: 10, eatingRate: 12})
 renderSettlement(settlement);
 
-setInterval(() => {
-  settlement = turn(settlement)
-  renderSettlement(settlement);
-}, 500);
+document.body.addEventListener('click', toggleLoop, true);
+
+function toggleLoop() {
+  loopOn = !loopOn;
+
+  if(loopOn) {
+    run();
+  } else {
+    clearTimeout(timeoutId)
+  }
+}
+
+function run() {
+  timeoutId = setInterval(() => {
+    settlement = turn(settlement)
+    if (settlement.people.count() === 0) {
+      toggleLoop();
+    }
+    renderSettlement(settlement);
+  }, delay);
+}
+
+run();
