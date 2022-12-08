37f74a4ee90d1dcc086823a80976d353f3b18472
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-08 21:38:28 +0100

    Simplify dev setup and prepare for TS

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-  root: 'src',
-  plugins: [
-    AutoImport({
-      imports: ['vitest'],
-      dts: true,
-    }),
-  ],
   test: {
     globals: true,
   },
